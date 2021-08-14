315d7e323a59f03c5ab99434d617d9c7ba007e17
cambridgew <cambridgew15@gmail.com>
2021-08-14 18:58:10 +0800

    新增抽奖管理器

---
+++ src/main/java/org/indiv/cambridgew/lottery/manager/DrawManager.java
@@ -0,0 +1,12 @@
+package org.indiv.cambridgew.lottery.manager;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 抽奖管理器
+ * @author cambridge.w
+ * @since 2021/8/14
+ */
+@Service
+public class DrawManager {
+}
